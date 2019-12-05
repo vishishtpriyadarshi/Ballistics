c61075c4e69f930e57efd7da4b3ffcd7ae57964e
vishishtpriyadarshi <vishishtpriyadarshi867@gmail.com>
2019-12-05 13:34:24 +0530

    Updates

--- particle.js
+++ particle.js
@@ -1,48 +1,59 @@
 var particle = {
-	position: null,
-	velocity: null,
+	x: 0,
+	y: 0,
+	vx: 0, 
+	vy: 0,
 	mass: 1,
 	radius: 0,
 	bounce: -1,
 	friction: 1,
+	gravity: 0,
 
 	create: function(x, y, speed, direction, grav) {
+		console.log("x: ", x);
 		var obj = Object.create(this);
-		obj.position = vector.create(x, y);
-		obj.velocity = vector.create(0, 0);
-		obj.velocity.setLength(speed);
-		obj.velocity.setAngle(direction);
-		obj.gravity = vector.create(0, grav || 0);
+		this.x = x;
+		this.y = y;
+		this.vx = Math.cos(direction) * speed;
+		this.vy = Math.sin(direction) * speed;
+		obj.gravity = grav || 0;
 		return obj;
 	},
 
-	accelerate: function(accel) {
-		this.velocity.addTo(accel);
+	accelerate: function(ax, ay) {
+		this.vx += ax;
+		this.vy += ay;
 	},
 
 	update: function() {
-		this.velocity.multiplyBy(this.friction);
-		this.velocity.addTo(this.gravity);
-		this.position.addTo(this.velocity);
+		this.vx *= this.friction;
+		this.vy *= this.friction;
+		this.vy += this.gravity;
+		this.x += this.vx;
+		this.y += this.vy;
 	},
 
 	angleTo: function(p2) {
-		return Math.atan2(p2.position.getY() - this.position.getY(), p2.position.getX() - this.position.getX());
+		return Math.atan2(p2.y - this.y, p2.x - this.x);
 	},
 
 	distanceTo: function(p2) {
-		var dx = p2.position.getX() - this.position.getX(),
-			dy = p2.position.getY() - this.position.getY();
+		var dx = p2.x - this.x,
+			dy = p2.y - this.y;
 
 		return Math.sqrt(dx * dx + dy * dy);
 	},
 
 	gravitateTo: function(p2) {
-		var grav = vector.create(0, 0),
-			dist = this.distanceTo(p2);
+		var dx = this.x - p2.x,
+			dy = this.y - p2.y,
+			distSQ = dx * dx + dy * dy,
+			distance = Math.sqrt(distSQ),
+			force = p2.mass / distSQ,
+			ax = dx / distance * force,
+			ay = dy / distance * force;
 
-		grav.setLength(p2.mass / (dist * dist));
-		grav.setAngle(this.angleTo(p2));
-		this.velocity.addTo(grav);
+		this.vx += ax;
+		this.vy += ay;
 	}
 };
